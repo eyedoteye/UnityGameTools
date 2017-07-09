8398345e2c26b6baf2d8f6d3a1047cb3573d9eb8
i.i <i.i@001.run>
2017-07-08 21:39:47 -0400

    Fix Quad Rotation Facing Camera Always Instead Of Being Parallel To Camera Side Axis [PerspectiveViewTransformTools]

--- Assets/Scripts/MonoBehavior/Tools/PerspectiveViewTransformTools.cs
+++ Assets/Scripts/MonoBehavior/Tools/PerspectiveViewTransformTools.cs
@@ -27,7 +27,8 @@ public class PerspectiveViewTransformTools : MonoBehaviour {
     else if(newQuadScale.z == 0)
       newQuadScale.z = 1;
 
-    Vector3 vectorPointingToCameraFromQuad = newQuadPosition - perspectiveCamera.transform.position;
+    Vector3 newQuadPositionBase = perspectiveCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, relativeDistance));
+    Vector3 vectorPointingToCameraFromQuad = perspectiveCamera.transform.position - newQuadPositionBase;
     Vector3 newQuadRotationEuler = Quaternion.LookRotation(vectorPointingToCameraFromQuad).eulerAngles;
     Vector3 perspectiveCameraRotationEuler = perspectiveCamera.transform.rotation.eulerAngles;
     newQuadRotation = Quaternion.Euler(newQuadRotationEuler.x, newQuadRotationEuler.y, perspectiveCameraRotationEuler.z);

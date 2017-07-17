78c951ff8980ab6d3b8b6f7f7062e87318c59b08
i.i <i.i@001.run>
2017-07-16 20:20:23 -0400

    Add Jumping

--- Assets/Scripts/MonoBehavior/Player.cs
+++ Assets/Scripts/MonoBehavior/Player.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-  public float moveSpeed;
+  public GameObject orientation;
+
+  public float rotationSpeed;
   public float jumpVelocity;
 
   private Vector2 leftJoystickInput;
@@ -27,20 +29,109 @@ public class Player : MonoBehaviour {
     leftJoystickInput.y = Input.GetAxis("Vertical");
 
     jumpInput = Input.GetButtonDown("Jump");
-
-    Debug.DrawRay(boxCollider.transform.position,
-                  Vector3.down * 16f,
-                  onGround ? Color.green : Color.white,
-                  0, false);
   }
 
   public void FixedUpdate()
   {
-    float yVelocity = rigidbody.velocity.y; 
-    rigidbody.velocity = boxCollider.transform.right * leftJoystickInput.x * moveSpeed;
-    rigidbody.velocity += Vector3.up * yVelocity;
+    rigidbody.angularVelocity =
+      transform.forward *
+      (-leftJoystickInput.x * rotationSpeed);
 
-    onGround = Physics.Raycast(transform.position, Vector3.down, 16f);
+    float angle;
+    Vector3 axis;
+    rigidbody.rotation.ToAngleAxis(out angle, out axis);
+
+    if(leftJoystickInput.x == 0 && angle % 90 < 2)
+    {
+      int mod = (int)(angle % 90);
+      int intangle = (int)(angle / 90);
+      angle = mod * intangle;
+      rigidbody.MoveRotation(Quaternion.AngleAxis(angle, axis));
+      rigidbody.angularVelocity = Vector3.zero;
+    }
+
+    Vector3 bottommostPoint = transform.position +
+      Vector3.down * transform.localScale.y / 2f;
+ 
+    if(angle % 90 != 0)
+    {
+      int quadrant = (int)(angle / 90) % 4;
+
+      int rightMod = 1;
+      int upMod = 1;
+      switch(quadrant)
+      {
+        case 0:
+        {
+          rightMod = -1;
+          upMod = -1;
+        } break;
+        case 1:
+        {
+          rightMod = -1;
+          upMod = 1;
+        } break;
+        case 2:
+        {
+          rightMod = 1;
+          upMod = 1;
+        } break;
+        case 3:
+        {
+          rightMod = 1;
+          upMod = -1;
+        } break;
+      } 
+
+      bottommostPoint = transform.position + 
+        Vector3.right * transform.localScale.x / 2f * rightMod +
+        Vector3.up * transform.localScale.y / 2f * upMod;
+      
+      Debug.Log(quadrant + " " + angle + " " + bottommostPoint);
+    }
+
+    float hypot = Mathf.Sqrt(
+      Mathf.Pow(transform.localScale.y / 2f, 2) +
+      Mathf.Pow(transform.localScale.x / 2f, 2)); 
+
+    onGround = Physics.Raycast(
+      transform.position, Vector3.down,
+      hypot + 1f);
+
+    Debug.DrawRay(
+      transform.position,
+      Vector3.down * (hypot + 1f),
+      onGround ? Color.green : Color.white, 0, false);
+
+
+    //float yVelocity = rigidbody.velocity.y; 
+    //rigidbody.velocity = Quaternion.Inverse(transform.rotation) * transform.right *
+    //                     (leftJoystickInput.x * moveSpeed);
+    //rigidbody.velocity += Vector3.up * yVelocity;
+    //if(leftJoystickInput.x == 0)
+    //  rigidbody.angularVelocity = new Vector3(0f,0f,0f);
+    //rigidbody.AddTorque(transform.forward * moveSpeed * -leftJoystickInput.x);
+
+    //float rightEdge = 1;
+    //if(leftJoystickInput.x > 0)
+    //  rightEdge = -1; 
+
+    //Vector3 edge = transform.position +
+    //  (Vector3.down + Vector3.right * rightEdge) * (transform.localScale.y / 2);
+
+    //edge = transform.rotation * edge; 
+
+    //bool rightEdgeOnGround = Physics.Raycast(edge, Vector3.down,
+    //                                         1f);
+    //Debug.DrawRay(edge, Vector3.down,
+    //              onGround ? Color.green : Color.white,
+    //              0, false);
+
+    //if(leftJoystickInput.x != 0)
+    //  rigidbody.MoveRotation(rigidbody.rotation *
+    //    Quaternion.Euler(transform.forward *
+    //    Mathf.Deg2Rad * rotationSpeed * rightEdge));
+
     if(jumpInput && onGround)
     {
       rigidbody.velocity += Vector3.up * jumpVelocity;
